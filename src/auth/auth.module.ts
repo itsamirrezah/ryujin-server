f0732b36a590dd1f547bd9c698df56b939a19858
Amirreza H <itsamirrezah@gmail.com>
2023-05-21 20:26:38 +0330

    add session management

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,18 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { LocalStrategy } from './passport/local.strategy';
+import { SessionSerialize } from './passport/session.serialize';
 import { AuthService } from './service/auth.service';
 import { HashingService } from './service/hashing.service';
 import { ScryptService } from './service/scrypt.service';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule.register({ session: true })],
   controllers: [AuthController],
   providers: [
     AuthService,
     LocalStrategy,
+    SessionSerialize,
     {
       provide: HashingService,
       useClass: ScryptService
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as session from 'express-session'
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {}
+  }))
+  app.use(passport.session())
   await app.listen(3000);
 }
 bootstrap();
