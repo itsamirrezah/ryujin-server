94def0959ef98f41d382e0614733017d94fb8dc4
Amirreza H <itsamirrezah@gmail.com>
2023-07-24 21:29:16 +0330

    store sessions in redis storage

--- src/common/custom-socketio-adapter.ts
+++ src/common/custom-socketio-adapter.ts
@@ -1,10 +1,19 @@
+import { INestApplicationContext, Injectable } from "@nestjs/common";
 import { IoAdapter } from "@nestjs/platform-socket.io"
 import { sessionMiddleware } from "src/main";
+import { RedisService } from "./redis.service";
 
+@Injectable()
 export class CustomSocketIoAdapter extends IoAdapter {
+  constructor(
+    app: INestApplicationContext,
+    private readonly redisService: RedisService
+  ) {
+    super(app)
+  }
   createIOServer(port: number, options?: any) {
     const server = super.createIOServer(port, options)
-    server.engine.use(sessionMiddleware)
+    server.engine.use(sessionMiddleware(this.redisService))
     return server
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as session from 'express-session'
 import { CustomSocketIoAdapter } from './common/custom-socketio-adapter';
+import { RedisService } from './common/redis.service';
+import { Redis } from 'ioredis';
+import RedisStore from 'connect-redis';
 
-export const sessionMiddleware = session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: null,
-  }
-})
+export function sessionMiddleware(redisClient: Redis) {
+  return session({
+    secret: process.env.SESSION_SECRET,
+    store: new RedisStore({ client: redisClient }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: null,
+    }
+  })
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: /http:\/\/.*:3000/, credentials: true })
-
   app.useGlobalPipes(new ValidationPipe())
-  app.use(sessionMiddleware)
-  app.useWebSocketAdapter(new CustomSocketIoAdapter(app))
+  const redisClient = app.get<RedisService>(RedisService)
+  const redisSession = sessionMiddleware(redisClient)
+  app.use(redisSession)
+  app.useWebSocketAdapter(new CustomSocketIoAdapter(app, redisClient))
   await app.listen(3001);
 }
 bootstrap();
